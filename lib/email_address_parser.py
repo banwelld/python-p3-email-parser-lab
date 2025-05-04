d942be227f53afdd7dbe4a9e98928c2c6fb3ef35
Dave Banwell <dave@davebanwell.com>
2025-05-04 14:22:37 -0400

    all tests passing

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,17 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, string):
+        self.string = string
+
+    
+    def parse(self):
+        pattern = r"\b[a-zA-Z](?:\w*|\w*\.\w+)@(?:\w+(?:\.\w+)*\w*)\.[a-zA-Z]+\b"
+        regex = re.compile(pattern)
+        return sorted(set(regex.findall(self.string)))
+        
+
+if __name__ == "__main__":
+    this = EmailAddressParser("talk@talk.com john.jones@flatironschool.com alexa@amazon.com")
+    output = this.parse()
+    print(output)
